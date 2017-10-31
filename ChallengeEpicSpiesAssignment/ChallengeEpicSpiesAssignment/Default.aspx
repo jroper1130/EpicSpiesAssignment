489547306bfda7ec2380daa0eed57b9318d0d908
jroper1130 <warbeast78@hotmail.com>
2017-10-31 02:50:56 -0700

    Add files via upload

---
+++ ChallengeEpicSpiesAssignment/ChallengeEpicSpiesAssignment/Default.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengeEpicSpiesAssignment.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 164px;
+            height: 190px;
+        }
+        .auto-style2 {
+            font-size: large;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <img alt="" class="auto-style1" src="download.png" /><br />
+            <br />
+            <strong><span class="auto-style2">Spy New Assingment Form</span></strong><br />
+            <br />
+            Spy Code Name: <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            New Assignment Name:<asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            End Date of Previous Assignment:<asp:Calendar ID="Calendar1" runat="server"></asp:Calendar>
+            <br />
+            Start Date of New Assignment:<asp:Calendar ID="Calendar2" runat="server"></asp:Calendar>
+            <br />
+            Projected End Date of New Assignment:<asp:Calendar ID="Calendar3" runat="server"></asp:Calendar>
+            <br />
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Assign Spy" />
+            <br />
+            <br />
+            <asp:Label ID="Label1" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ChallengeEpicSpiesAssignment/ChallengeEpicSpiesAssignment/Default.aspx.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeEpicSpiesAssignment
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //rule1
+            if (!Page.IsPostBack)
+            {
+                Calendar1.SelectedDate = DateTime.Now.Date;
+                Calendar2.SelectedDate = DateTime.Now.Date.AddDays(14);
+                Calendar3.SelectedDate = DateTime.Now.Date.AddDays(21);
+            }
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //rule3
+            TimeSpan doa = Calendar3.SelectedDate.Subtract(Calendar2.SelectedDate);
+            double tc = doa.TotalDays * 500.0;
+
+            //rule4
+            if (doa.TotalDays > 21)
+            {
+                tc += 1000.0;
+            }
+            //this isnt showing up at all
+            Label1.Text = String.Format("Assignment of {0} to the assignment  {1} is authorized the amount of {2:C}", TextBox1.Text, TextBox2.Text, tc);
+            
+            //rule2
+            TimeSpan tba = Calendar2.SelectedDate.Subtract(Calendar1.SelectedDate);
+            if (tba.TotalDays < 14)
+            {
+                Label1.Text = "Error, must allow at least 2 weeks in between assignmetns.";             
+            }
+            DateTime enad = Calendar1.SelectedDate.AddDays(14);
+            Calendar2.SelectedDate = enad;
+            Calendar2.VisibleDate = enad;
+        }
+    }
+}
+/*
+Rule1:
+first calender set to today.
+second calender set 14 days from today.
+third calender set 21 days from today.
+
+Rule2:
+Must be 14 days, if not selected correctly a error msg will show up.
+Also show earliest available date on calender by selecting and showing 
+date in the second calendar.
+
+1= if not 14 days(error msg) else(concenated code)
+2= 
+
+Rule3:
+Cost $500 a day; display in label.
+Rule4:
+ If assignment is  over 3 weeks, and extra $1000 will be added.*/
